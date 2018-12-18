6835dea339bc054311bdab22f774e4021b26cdad
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-18 09:23:03 +0100

    aufgabe 3

---
+++ src/Aufgabe_3/Auto.java
@@ -0,0 +1,96 @@
+package Aufgabe_3;
+
+
+
+public class Auto extends Fahrzeug implements Comparable<Auto>
+    /* Comparable muss impletiert werden um die Methode compareTo zu überschreiben
+  mit <Auto> vergleicht man zwei Autos
+*/
+
+{
+    private boolean klimaanlage;
+    private short airbag;
+
+
+
+    public void klimaanlageAn()
+    {
+        if(this.klimaanlage==true)
+        {
+            System.out.println("Klimaanlage ist bereits an.");
+        }
+        else
+        {
+            this.klimaanlage = true;
+        }
+    }
+
+    public void klimaanlageAus()
+    {
+        if(this.klimaanlage==false)
+        {
+            System.out.println("Klimaanlage ist bereits aus.");
+        }
+        else
+        {
+            this.klimaanlage = false; //könnte auch getter und setter verwenden
+
+        }
+    }
+
+    //aufgabe3
+
+    public boolean getKlimaanlage() {
+        return klimaanlage;
+    }
+
+    public void setKlimaanlage(boolean klimaanlage) {
+        this.klimaanlage = klimaanlage;
+    }
+
+    public short getAirbag() {
+        return airbag;
+    }
+
+    public void setAirbag(short airbag) {
+        this.airbag = airbag;
+    }
+
+    //aufgabe 4
+
+    public Auto( short airbag, short reifen, String color, short ps, short tueren, short geschwindigkeit) {
+        super(reifen, color, ps, tueren, geschwindigkeit);
+        this.klimaanlage = false;
+        this.airbag = airbag;
+    }
+
+    //aufgabe 6 Methode überschreiben
+
+    @Override
+    public String toString(){
+        return "Mein Auto hat "+this.getPs()+" PS und fährt mit "+this.getGeschwindigkeit()+" km/h";
+    }
+
+
+    @Override
+    public int compareTo(Auto o)
+    {
+        if (this.getPs() == o.getPs())
+        {
+            return 0;
+        }
+
+        if (this.getPs() < o.getPs()){
+            return 1;
+        }
+        return -1;
+    }
+
+
+        //Wenn das erste Auto mehr PS hat als das zweite Auto wird es in der liste drübergestellt deshalb return 1
+        //ist es kleiner darunter deshalb -1
+        //bei gleicher ps werden sie gleichgestellt deshalb return 0
+
+
+}
+
--- src/Aufruf.java
+++ src/Aufruf.java
@@ -1,5 +1,10 @@
 import Aufgabe_1.Mensch;
 import Aufgabe_2.Student;
+import Aufgabe_3.Auto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Aufruf
 {
@@ -19,6 +24,30 @@ public class Aufruf
 
         Student larissa2 = new Student("Larissa", "Blachfelder", 19, 1810653996, "Web Business & Technology");
         larissa2.Status();
+
+        //Aufgabe 3
+        System.out.println("\nAufgabe 3:");
+
+        Auto bmw = new Auto ((short)4, (short)4, "blau", (short)220, (short)4, (short)200);
+        Auto audi = new Auto ((short)6, (short)4, "weiß", (short)180, (short)3, (short)150);
+        Auto renault = new Auto ((short)4, (short)4, "schwarz", (short)200, (short)5, (short)70);
+        Auto suzuki = new Auto ((short)8, (short)4, "grau", (short)150, (short)4, (short)190);
+        Auto audi2 = new Auto ((short)7, (short)4, "rot", (short)160, (short)5, (short)230);
+
+        //liste mit autos
+        List<Auto> liste = new ArrayList<>();
+        liste.add(bmw);
+        liste.add(audi);
+        liste.add(renault);
+        liste.add(suzuki);
+        liste.add(audi2);
+        Collections.sort(liste);//liste der autos sortieren nach PS absteigend
+
+        for (Auto i : liste)
+        {
+            System.out.println(i);
+        }
+
 
 
     }
